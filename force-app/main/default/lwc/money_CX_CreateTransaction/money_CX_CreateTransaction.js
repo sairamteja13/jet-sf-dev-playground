6bca8c2bb1a6213184524fa6e70c913a72fbcb87
Sai Ram Teja <sairamteja413@gmail.com>
2025-12-23 21:26:57 +0530

    created componenet for transaction creation

---
+++ force-app/main/default/lwc/money_CX_CreateTransaction/money_CX_CreateTransaction.js
@@ -0,0 +1,46 @@
+import { LightningElement } from 'lwc';
+import userId from '@salesforce/user/Id';
+import AMOUNT_FIELD from '@salesforce/schema/Transaction__c.Transaction_Amount__c';
+import DATE_FIELD from '@salesforce/schema/Transaction__c.Transaction_Date__c';
+import TYPE_FIELD from '@salesforce/schema/Transaction__c.Type_of_Transaction__c';
+import DESCRIPTION_FIELD from '@salesforce/schema/Transaction__c.Description__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Money_CX_CreateTransaction extends LightningElement {
+    objectApiName = 'Transaction__c';
+    amount = AMOUNT_FIELD;
+    date = DATE_FIELD;
+    type = TYPE_FIELD;
+    description = DESCRIPTION_FIELD;
+    // amount;
+    // date;
+    // type;
+    // description;
+    userId = userId;
+
+    handleChange(event) {
+        const field = event.target.name;
+        if (field === 'amount') {
+            this.amount = event.target.value;
+        } else if (field === 'date') {
+            this.date = event.target.value;
+        } else if (field === 'type') {
+            this.type = event.target.value;
+        } else if (field === 'description') {
+            this.description = event.target.value;
+        }
+    }
+
+    handleSubmit(event) {
+        const transactionId = event.detail.id;
+        console.log('Transaction created with Id: ' + transactionId);    
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Transaction created successfully!',
+                variant: 'success',
+            }),
+        );
+        event.preventDefault();
+    }
+}
